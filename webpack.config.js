0454468dc710809cf8fa4d1deddc808307521517
yogiman <xuyuji9000@163.com>
2017-02-09 09:58:52 +0800

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,18 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'build'),
         filename: 'bundle.js',
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
     }
 };
